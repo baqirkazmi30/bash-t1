de7ab5a1c2f7afb730ddb2efc850fe21f8b67fed
baqirkazmi30 <baqirkazmi30@gmail.com>
2018-10-01 10:55:36 +0500

    Final commit

--- bash_task.sh
+++ bash_task.sh
@@ -7,26 +7,24 @@ ovs_installation()
 #echo "-------------------------------"
 #echo "Step 1. Install git package"
 #echo "-------------------------------"
-sudo apt-get update >> update.log
-sudo apt-get -y install git >> gitinstll.log
+sudo apt-get update >> output.txt
+sudo apt-get -y install git >> output.txt
 
 #echo "Step 2. Clone Openvswitch git Repo"
-git clone https://github.com/baqirkazmi30/ovs.git >> ovs_clone.log
+git clone --progress  https://github.com/baqirkazmi30/ovs.git &>> output.txt
 
 #echo "Step 3. Install tools"
-sudo apt-get -y install autoconf libtool make >> output_lib.log
+sudo apt-get -y install autoconf libtool make &>> output.txt
 
 #echo "Step 4.  Build OvS"
 cd ovs/
-./boot.sh >> boot.txt
-./configure --with-linux=/lib/modules/`uname -r`/build >> configure.log
-
-make >> make.log
-
+./boot.sh >> output.txt 2>&1  #&>> output.txt
+./configure --with-linux=/lib/modules/`uname -r`/build >> output.txt 2>&1 # &>> output.txt
+make >> output.txt 2>&1 # &>> output.txt
 b=$(echo $?)
 #T=$(date +%R)
 T=$(date +%d-%m-%Y,Time:%H-%M-%S)
-echo "$b"
+#echo "$b"
 
 if [ $b -eq 0 ]; 
 then
@@ -43,12 +41,12 @@ fi
 #sudo make install 
 return
 
-sudo make install >> output_mak.log
-sudo make modules_install >> output_mod.log
+sudo make install >> output.txt 2>&1 #&>> output.txt
+sudo make modules_install >> output.txt 2>&1 #&>> output.txt
  
 
 #echo "Step 6. Create OvS Database"
-sudo ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema >> db.log
+sudo ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema &>> output.txt
 
 #echo "Step 7. Run OvS..."
 sudo ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
@@ -56,23 +54,23 @@ sudo ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
                  --private-key=db:Open_vSwitch,SSL,private_key \
                  --certificate=db:Open_vSwitch,SSL,certificate \
                  --bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert \
-                 --pidfile --detach >> ovs_server.log
-sudo ovs-vsctl --no-wait >> ovs_vsctl.log
-sudo ovs-vswitchd --pidfile --detach  >> ovs_vssw.log
+                 --pidfile --detach 
+sudo ovs-vsctl --no-wait 
+sudo ovs-vswitchd --pidfile --detach 
 
 
 }
 
 timer()
 {
-   echo "30 sec timer"
+   #echo "30 sec timer"
     sleep 1
      for  (( i=30; i>0; i-- ))
 #     for  (( i=10; i>0; i-- ))
     do
        sleep 1
      
-     echo -ne "Wait for $i Sec"'\r'
+    # echo -ne "Wait for $i Sec"'\r'
     done
 }
 
@@ -92,8 +90,8 @@ do
 	if [ $LOCAL = $REMOTE ]; then
 	echo "Up-to-date" 
 	else
-	echo "Installing OVS"
-        rm -r ovs  
+	#echo "Installing OVS"
+        rm -rf ovs >> output.txt
 	ovs_installation
         echo "Commit id: $a"
  	fi
